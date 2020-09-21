1630a14ff9fac10dfff0808acdaa013ea1df8b4b
Venice Vargas <venicevargas1@live.com>
2020-09-21 02:46:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,35 +1,23 @@
-class Scraper
+ @@all = []
 
-  def self.scrape_index_page(index_url)
+  def initialize(student_hash)
 
-    students_hash = []
-    html = Nokogiri::HTML(open(index_url))
-    html.css(".student-card").collect do |student|
-      hash = {
-        name: student.css("h4.student-name").text,
-        location: student.css("p.student-location").text,
-        profile_url: "http://students.learn.co/" + student.css("a").attribute("href")
-      }
-      students_hash << hash
-    end
-    students_hash
+    student_hash.each {|k,v| self.send(("#{k}="),v)}
+    @@all << self
   end
 
-  def self.scrape_profile_page(profile_url)
+  def self.create_from_collection(students_array)
 
+    students_array.each{|student| Student.new(student)}
   end
-    students_hash = {}
 
-    html = Nokogiri::HTML(open(profile_url))
-    html.css("div.social-icon-controler a").each do |student|
-        url = student.attribute("href")
-        students_hash[:twitter_url] = url if url.include?("twitter")
-        students_hash[:linkedin_url] = url if url.include?("linkedin")
-        students_hash[:github_url] = url if url.include?("github")
-        students_hash[:blog_url] = url if student.css("img").attribute("src").text.include?("rss")
-    end
-        students_hash[:profile_quote] = html.css("div.profile-quote").text
-        students_hash[:bio] = html.css("div.bio-content p").text
-    students_hash
+  def add_student_attributes(attributes_hash)
+
+    attributes_hash.each{|k,v| self.send(("#{k}="),v)}
+  end
+
+  def self.all
+
+    @@all
   end
 end
